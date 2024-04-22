74c61075e668ee177ad6500c2fc6583444e0b1f1
khanjannat <160754979+khanjannat@users.noreply.github.com>
2024-04-22 18:07:44 +0500

    Done

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/in/env node
+// this line is called a shebhang, it tells the OS to run it with nodejs
+
+// import the 'inquirer' module, which is a command line interface for node.js
+import inquirer from "inquirer";
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
